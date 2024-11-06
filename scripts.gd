a27df97654663bb4c98eb6646c7909759b55c8d7
Guilherme Teodoro <teo827@hotmail.com>
2024-11-06 00:07:13 -0300

    up

---
+++ scripts.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var garv = 10
+var speed = 300
+
+
+func _process(delta):
+	
+	if !is_on_floor():
+		velocity.y += garv
+		
+		if Input.is_action_pressed("ui_right"):
+			velocity.x =speed
+			$Sprite2D.flip_h = false
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = -speed
+			$Sprite2D.flip_h = true
+		else:
+			velocity.x = 0
+			
+		if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+			velocity.y -= 300
+
+		move_and_slide()
+
+func _ready():
+	print("Olá Montanha, seu Aluno Guilherme que fez, não é nada d+, peço desculpas pela simplicidade")
